ace9c7302d271fd78b31574ebef971ce0e9b3d9f
Graham Rowlands <grahamrow@gmail.com>
2016-02-26 14:33:06 -0500

    Safer application of regex

--- quince/util.py
+++ quince/util.py
@@ -9,20 +9,20 @@ import re
 def next_available_name(node_names, label):
 	# Check node_names and see if we can find 'label ###'
 	# If we can, then we'll return the next available ###+1
-	match_attempts = [re.match(label+' (\d+)', nn) for nn in node_names]
 	try:
+		match_attempts = [re.match(label+' (\d+)', nn) for nn in node_names]
 		match_attempts[:] = [int(m.group(1)) for m in match_attempts if m != None]
 		if len(match_attempts) == 0:
 			return label+" 1"
 		else:
 			return label+" {:d}".format(sorted(match_attempts)[-1]+1)
 	except:
-		return label + '???'
+		return label + ' ?'
 
 def strip_numbers(label):
-	match_attempt = re.match('(.*?)(\d+)$', label)
 	try:
+		match_attempt = re.match('(.*?)(\d+)$', label)
 		stripped = match_attempt.group(1).strip()
 	except:
-		stripped = label + '???'
+		stripped = label
 	return stripped
