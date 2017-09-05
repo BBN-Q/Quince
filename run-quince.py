366ecdd77520004c307cbbf127bb374ab546ce7e
Graham Rowlands <grahamrow@gmail.com>
2017-09-05 10:56:29 -0400

    Use windows API to change the AppID and use our icon.

--- run-quince.py
+++ run-quince.py
@@ -12,6 +12,7 @@ os.environ["QT_API"] = 'pyqt5'
 from qtpy.QtWidgets import QApplication
 import sys
 import argparse
+import ctypes
 
 from quince.view import *
 
@@ -27,6 +28,12 @@ if __name__ == "__main__":
     # Setup icon
     png_path = os.path.join(os.path.dirname(__file__), "assets/quince_icon.png")
     app.setWindowIcon(QIcon(png_path))
+
+    # Convince windows that this is a separate application to get the task bar icon working
+    # https://stackoverflow.com/questions/1551605/how-to-set-applications-taskbar-icon-in-windows-7/1552105#1552105
+    if (os.name == 'nt'):
+        myappid = u'BBN.quince.gui.0001' # arbitrary string
+        ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
 
     window = NodeWindow()
     window.load_yaml(args.filename)
