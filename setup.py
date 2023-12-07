4f6d3159437307b7db32f457d9d737b4ec67adb2
Michael Mendy <montana@linux.com>
2023-12-06 22:27:38 -0800

    `yapf` to PEP8 standards.

--- setup.py
+++ setup.py
@@ -1,30 +1,27 @@
 from setuptools import setup
 import os
 
-if os.name == 'nt':
+if os.name == "nt":
     # Find the path
     path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "run-quince.py")
 
     # Create the run-quince.bat script
-    with open('run-quince.bat', 'w') as f:
+    with open("run-quince.bat", "w") as f:
         f.write("python {} %*\n".format(path))
 
-    script_names = ['run-quince.bat']
+    script_names = ["run-quince.bat"]
 else:
-    script_names = ['run-quince.py']
+    script_names = ["run-quince.py"]
 
 setup(
-    name='quince',
-    version='0.1',
-    author='Quince Developers',
+    name="quince",
+    version="0.1",
+    author="Quince Developers",
     # package_dir={'':'quince'},
-    packages=['quince'],
+    packages=["quince"],
     scripts=script_names,
     data_files=["assets"],
-    description='Quince is a node centric experience.',
-    long_description=open('README.md').read(),
-    install_requires=[
-        "Numpy >= 1.6.1",
-        "QtPy >= 1.1.2"
-    ]
+    description="Quince is a node centric experience.",
+    long_description=open("README.md").read(),
+    install_requires=["Numpy >= 1.6.1", "QtPy >= 1.1.2"],
 )
